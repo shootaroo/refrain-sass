a150f0bdc9296560101d5cb2f39ab3392bba4efa
shootaroo <shotaro.tsubouchi@gmail.com>
2015-11-05 16:15:15 +0900

    Support options

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-'use strict';
-var sass = require('node-sass');
-var extend = require('xtend');
+import sass from 'node-sass';
+import extend from 'xtend';
 
-module.exports = (text, content, callback) => {
-  sass.render(extend({}, content, {
+export default function (text, content, callback) {
+  sass.render(extend(this.options.sass || {}, content, {
     data: text
   }), (err, result) => {
     callback(
       err ? `Error: ${err.message}\n    at ${err.file}:${err.line}:${err.column}` : err,
       result ? result.css : result);
   });
-};
+}
--- test/test.js
+++ test/test.js
@@ -1,10 +1,9 @@
 /* global it */
-'use strict';
-var assert = require('power-assert');
-var target = require('..');
+import assert from 'power-assert';
+import target from '../src';
 
 it('success', done => {
-  target('.foo {.bar {color:#aaa}}', {}, (err, output) => {
+  target.call({options: {}}, '.foo {.bar {color:#aaa}}', {}, (err, output) => {
     assert(!err);
     assert(output.toString() === '.foo .bar {\n  color: #aaa; }\n');
     done();
@@ -12,8 +11,8 @@ it('success', done => {
 });
 
 it('error', done => {
-  target('.foo {.bar color:#bbb}}', {}, err => {
-    assert(err === 'Error: invalid property name\n    at stdin:1:6');
+  target.call({options: {}}, '.foo {.bar color:#bbb}}', {}, err => {
+    assert(err);
     done();
   });
 });
